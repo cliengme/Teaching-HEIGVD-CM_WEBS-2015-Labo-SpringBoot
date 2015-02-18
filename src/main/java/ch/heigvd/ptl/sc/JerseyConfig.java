b28b178871c3bf183b367769b5bdc27f8da17bf2
Cedric <cliengme@gmail.com>
2015-02-18 15:43:29 +0100

    fix import in JerseyConfig

--- src/main/java/ch/heigvd/ptl/sc/JerseyConfig.java
+++ src/main/java/ch/heigvd/ptl/sc/JerseyConfig.java
@@ -2,6 +2,7 @@ package ch.heigvd.ptl.sc;
 
 import ch.heigvd.ptl.sc.rest.UserResource;
 import ch.heigvd.ptl.sc.rest.DataResource;
+import ch.heigvd.ptl.sc.rest.IssueTypeResource;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
